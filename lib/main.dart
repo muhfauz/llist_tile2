7a84229316175dfce8107351f2d806b103671674
Sutrisno <sutrisno.stz@bsi.ac.id>
2022-10-26 22:13:10 +0700

    buat statefull widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,29 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('List Tile Klik'),
+        ),
+        body: Container(
+          margin: EdgeInsets.all(10),
+          child: ListView(
+            children: <Widget>[],
+          ),
+        ),
+      ),
+    );
+  }
+}
